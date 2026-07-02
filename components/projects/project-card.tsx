d2397d7d1a4d83ce309c8ce1cca37fc354751e73
Jeremiah Gage <jeremiahdgage@outlook.com>
2026-07-01 22:57:34 -0500

    Tune portfolio card presentation

--- components/projects/project-card.tsx
+++ components/projects/project-card.tsx
@@ -122,6 +122,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ project, handleClick }) => {
       }
       style={themeStyle}
       data-portfolio-theme={project.name}
+      data-portfolio-has-dark-image={project.theme?.cardDarkSrc ? "true" : "false"}
       className={cardClassName}
     >
       {project.theme?.css && (
@@ -136,7 +137,7 @@ const ProjectCard: FC<ProjectCardProps> = ({ project, handleClick }) => {
             </span>
             <div className="min-w-0">
               <TypographyH3
-                className="break-words leading-tight"
+                className="portfolio-card-title break-words leading-tight"
                 style={{ fontSize: getTitleSize(project.name) }}
               >
                 {project.name}
